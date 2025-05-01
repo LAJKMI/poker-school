fc70fa6af0ea5caa435e88391832751bde72331e
David <162476979+LAJKMI@users.noreply.github.com>
2025-05-01 16:02:51 +0200

    napravljen scenario page

--- pokerSchool/src/scenarioQuizz/Scenario.tsx
+++ pokerSchool/src/scenarioQuizz/Scenario.tsx
@@ -1,6 +1,51 @@
+import { Link } from 'react-router-dom'
+import navLogo from '../assets/pictures/pokerLogo.png'
+import pokerTable from '../assets/pictures/pokerTable.png'
+import './scenario.css'
 
 export default function Scenario() {
     return (
-        <div>Scenario</div>
+        <>
+            <header id="quizzHeader">
+                <Link to={'/'}><img src={navLogo} alt="logo" /></Link>
+                <Link to={'/'}>
+                    <button>Go Home</button>
+                </Link>
+            </header>
+            <div className="pokerTableContainer">
+                <img src={pokerTable} alt="poker table" id='pokerTable' />
+            </div>
+            <div className="scenarioContainer">
+                <div className="scenarioBox">
+                    <p>scenario 1/15</p>
+                    <p className='scenario'>You're on the button with King♥ Queen♥. A middle position player opens to $8 in a $1/$2 game. Everyone folds to you.</p>
+                </div>
+                <div className="choicesBox">
+                    <div className="choice">
+                        <input type="radio" name="fold" id="fold" />
+                        <label htmlFor="fold">
+                            Fold
+                        </label>
+                    </div>
+                    <div className="choice">
+                        <input type="radio" name="raise" id="raise" />
+                        <label htmlFor="raise">
+                            Raise
+                        </label>
+                    </div>
+                    <div className="choice">
+                        <input type="radio" name="check" id="check" />
+                        <label htmlFor="check">
+                            Check
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div className="btnBox">
+                <button className="nextScenario">
+                    Next
+                </button>
+            </div>
+        </>
     )
 }
